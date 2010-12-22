0874a8e2d2332bb59cfba021e3bec1436213ab5d
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-22 01:05:36 +0000

    Add jQuery 1.4.4 / Small bugfix (#10984)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jfcloudzoom/trunk@41367 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "jfcloudzoom".
 #
-# Auto generated 03-11-2010 23:19
+# Auto generated 22-12-2010 02:05
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.2',
+	'version' => '1.1.3',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:28:{s:24:"class.tx_jfcloudzoom.php";s:4:"58a6";s:12:"ext_icon.gif";s:4:"1eaf";s:17:"ext_localconf.php";s:4:"19e6";s:14:"ext_tables.php";s:4:"ce27";s:14:"ext_tables.sql";s:4:"04d7";s:15:"flexform_ds.xml";s:4:"05c7";s:13:"locallang.xml";s:4:"ab6c";s:16:"locallang_db.xml";s:4:"58f0";s:12:"mode_dam.gif";s:4:"999b";s:15:"mode_damcat.gif";s:4:"2596";s:15:"mode_folder.gif";s:4:"9d05";s:15:"mode_upload.gif";s:4:"fecd";s:12:"t3jquery.txt";s:4:"8543";s:14:"doc/manual.sxw";s:4:"c11e";s:39:"lib/class.tx_jfcloudzoom_cms_layout.php";s:4:"346c";s:42:"lib/class.tx_jfcloudzoom_itemsProcFunc.php";s:4:"224c";s:14:"pi1/ce_wiz.gif";s:4:"f039";s:32:"pi1/class.tx_jfcloudzoom_pi1.php";s:4:"78ef";s:40:"pi1/class.tx_jfcloudzoom_pi1_wizicon.php";s:4:"fa09";s:17:"pi1/locallang.xml";s:4:"7e03";s:33:"res/jquery/js/jquery-1.4.2.min.js";s:4:"1009";s:33:"res/jquery/js/jquery-1.4.3.min.js";s:4:"e495";s:39:"res/jquery/js/jquery.cloudzoom-1.0.2.js";s:4:"71ff";s:43:"res/jquery/js/jquery.cloudzoom-1.0.2.min.js";s:4:"c25f";s:20:"static/constants.txt";s:4:"364b";s:16:"static/setup.txt";s:4:"947a";s:24:"static/tt_news/setup.txt";s:4:"2f6d";s:28:"static/tt_products/setup.txt";s:4:"fdb4";}',
+	'_md5_values_when_last_written' => 'a:29:{s:24:"class.tx_jfcloudzoom.php";s:4:"58a6";s:12:"ext_icon.gif";s:4:"1eaf";s:17:"ext_localconf.php";s:4:"19e6";s:14:"ext_tables.php";s:4:"ce27";s:14:"ext_tables.sql";s:4:"04d7";s:15:"flexform_ds.xml";s:4:"05c7";s:13:"locallang.xml";s:4:"ab6c";s:16:"locallang_db.xml";s:4:"58f0";s:12:"mode_dam.gif";s:4:"999b";s:15:"mode_damcat.gif";s:4:"2596";s:15:"mode_folder.gif";s:4:"9d05";s:15:"mode_upload.gif";s:4:"fecd";s:12:"t3jquery.txt";s:4:"8543";s:14:"doc/manual.sxw";s:4:"8c33";s:39:"lib/class.tx_jfcloudzoom_cms_layout.php";s:4:"346c";s:42:"lib/class.tx_jfcloudzoom_itemsProcFunc.php";s:4:"224c";s:14:"pi1/ce_wiz.gif";s:4:"f039";s:32:"pi1/class.tx_jfcloudzoom_pi1.php";s:4:"e390";s:40:"pi1/class.tx_jfcloudzoom_pi1_wizicon.php";s:4:"fa09";s:17:"pi1/locallang.xml";s:4:"7e03";s:33:"res/jquery/js/jquery-1.4.2.min.js";s:4:"1009";s:33:"res/jquery/js/jquery-1.4.3.min.js";s:4:"e495";s:33:"res/jquery/js/jquery-1.4.4.min.js";s:4:"73a9";s:39:"res/jquery/js/jquery.cloudzoom-1.0.2.js";s:4:"71ff";s:43:"res/jquery/js/jquery.cloudzoom-1.0.2.min.js";s:4:"c25f";s:20:"static/constants.txt";s:4:"d45d";s:16:"static/setup.txt";s:4:"947a";s:24:"static/tt_news/setup.txt";s:4:"2f6d";s:28:"static/tt_products/setup.txt";s:4:"fdb4";}',
 	'suggests' => array(
 	),
 );
